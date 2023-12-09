8435f9446066dd63d3f87719b205a4451618adc9
pham ha <phamtuanha21092003@gmail.com>
2023-12-09 18:04:02 +0700

    config docker

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -3,3 +3,5 @@ import 'dotenv/config'
 export const ACCESS_TOKEN_KEY = process.env.ACCESS_TOKEN_KEY
 export const REFRESH_TOKEN_KEY = process.env.REFRESH_TOKEN_KEY
 export const REDIS_PORT = process.env.REDIS_PORT
+export const REDIS_HOST = process.env.REDIS_HOST
+export const REDIS_PASSWORD = process.env.REDIS_PASSWORD
--- src/utils/redis/index.ts
+++ src/utils/redis/index.ts
@@ -1,4 +1,12 @@
 import { Redis } from 'ioredis'
-import { REDIS_PORT } from '@configs'
+import { REDIS_PORT, REDIS_PASSWORD, REDIS_HOST } from '@configs'
 
-export const redis = new Redis(REDIS_PORT)
+export const redis = new Redis({
+    port: +REDIS_PORT,
+    host: REDIS_HOST,
+    password: REDIS_PASSWORD,
+})
+
+redis.on('connect', function () {
+    console.log('connect successfully')
+})
